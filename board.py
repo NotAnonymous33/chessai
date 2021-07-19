4da11ba4c678966988f8ec820c6eb3d0cb32ca3b
Ismail Choudhury <ismail.choudhury33@gmail.com>
2021-07-19 16:59:12 +0100

    Initial commit

---
+++ board.py
@@ -0,0 +1,17 @@
+import pygame
+from constants import *
+
+pygame.init()
+
+
+class Cell:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
+    def draw(self):
+        pass
+
+
+class Board:
+    pass
