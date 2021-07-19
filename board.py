f6a32dbc56e2a1bf386f07831de1078b95e7373f
Ismail Choudhury <ismail.choudhury33@gmail.com>
2021-07-19 17:23:39 +0100

    Finish board

--- board.py
+++ board.py
@@ -5,13 +5,22 @@ pygame.init()
 
 
 class Cell:
-    def __init__(self):
-        self.x = 0
-        self.y = 0
+    def __init__(self, window, coor):
+        self.x = coor[0]
+        self.y = coor[1]
+        self.color = [RCOLOR, LCOLOR][(self.x + self.y) % 2]
+        self.window = window
 
     def draw(self):
-        pass
+        pygame.draw.rect(self.window, self.color, [self.x * WIDTH, self.y * WIDTH, WIDTH, WIDTH])
 
 
 class Board:
-    pass
+    def __init__(self, window):
+        self.cells = [[Cell(window, (i, j)) for j in range(8)] for i in range(8)]
+        self.window = window
+
+    def draw(self):
+        for row in self.cells:
+            for cell in row:
+                cell.draw()
--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+import pygame
+from board import Board
+from constants import *
+
+pygame.init()
+
+WIN = pygame.display.set_mode((LENGTH, LENGTH))
+
+board = Board(WIN)
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    board.draw()
+pygame.quit()
+
 
 
 
