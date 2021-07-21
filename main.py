f17cfb296967ebf13ec27ccab9b3cb6bbeefe8fb
Ismail Choudhury <ismail.choudhury33@gmail.com>
2021-07-22 00:53:41 +0100

    Add main function and fix bugs

--- main.py
+++ main.py
@@ -4,35 +4,36 @@ from constants import *
 
 pygame.init()
 
-clock = pygame.time.Clock()
 
-board = Board()
+def main():
+    clock = pygame.time.Clock()
 
-running = True
+    board = Board()
 
-pygame.display.set_caption("Chess by Ismail Choudhury")
+    running = True
+
+    pygame.display.set_caption("Chess by Ismail Choudhury")
+
+    # game loop
+    while running:
+        pos = None
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+
+        if pos is not None:
+            board.clicked(pos)
+
+        # drawing
+        board.draw()
+        pygame.display.flip()
+
+        clock.tick(FPS)
 
 
-# game loop
-while running:
-    pos = None
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONUP:
-            pos = pygame.mouse.get_pos()
-
-    if pos is not None:
-        board.clicked(pos)
-
-
-
-    # drawing
-    board.draw()
-    pygame.display.flip()
-
-
-    clock.tick(FPS)
+main()
 
 pygame.quit()
 
