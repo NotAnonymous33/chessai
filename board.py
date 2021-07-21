c790b9d298061bd117321baca7adf41473f1be5b
Ismail Choudhury <ismail.choudhury33@gmail.com>
2021-07-21 02:26:01 +0100

    Add pawns to board. Fix selecting cells. Add Piece class and related enums

--- board.py
+++ board.py
@@ -1,7 +1,36 @@
 import pygame
 from constants import *
+from abc import ABC, abstractmethod
+from enum import Enum, auto
 
 pygame.init()
+
+class PieceColor(Enum):
+    BLACK = "b"
+    WHITE = "w"
+
+class PieceType(Enum):
+    PAWN = "p"
+    ROOK = "R"
+    BISHOP = "B"
+    KNIGHT = "N"
+    QUEEN = "Q"
+    KING = "K"
+
+class Piece:
+    def __init__(self, window, piece_color: PieceColor, piece_type: PieceType, coor: tuple):
+        self.window = window
+        self.piece_color = piece_color
+        self.piece_type = piece_type
+        self.xcoor = coor[0]
+        self.ycoor = coor[1]
+        self.image = IMAGES[self.piece_color.value + self.piece_type.value]
+
+
+
+    def draw(self):
+        self.window.blit(self.image, (self.xcoor * CLENGTH, self.ycoor * CLENGTH))
+
 
 
 class Cell:
@@ -15,6 +44,7 @@ class Cell:
         self.color = [LCOLOR, RCOLOR][(self.x + self.y) % 2]
         self.window = window
         self.selected = False
+        self.piece = None
 
     def draw(self):
         pygame.draw.rect(self.window, [self.color, SCOLOR][self.selected], [self.x * CLENGTH, self.y * CLENGTH, CLENGTH, CLENGTH])
@@ -22,23 +52,33 @@ class Cell:
 
 class Board:
     def __init__(self, window: pygame.display):
-        self.extra = Cell(window, (9, 9))
         self.cells = [[Cell(window, (i, j)) for j in range(8)] for i in range(8)]
         self.window = window
-        self.current_selected = self.extra
+        self.pieces = [
+            [],
+            [Piece(self.window, PieceColor.BLACK, PieceType.PAWN, (i, 1)) for i in range(8)],
+            [],
+            [],
+            [],
+            [],
+            [Piece(self.window, PieceColor.WHITE, PieceType.PAWN, (i, 6)) for i in range(8)],
+            []
+        ]
 
     def draw(self):
         for row in self.cells:
             for cell in row:
                 cell.draw()
+        for row in self.pieces:
+            for piece in row:
+                piece.draw()
+
 
     def set_selected(self, pos: tuple):
         for row in self.cells:
             for cell in row:
                 if cell.xcor1 < pos[0] < cell.xcor2 and cell.ycor1 < pos[1] < cell.ycor2:
-                    if self.current_selected == cell:
-                        cell.selected = False
-                    else:
-                        self.current_selected.selected = False
-                        cell.selected = True
-                        self.current_selected = cell
+                    cell.selected = not cell.selected
+                else:
+                    cell.selected = False
+
--- constants.py
+++ constants.py
@@ -1,6 +1,12 @@
+import pygame
+pygame.init()
+
 LCOLOR = (240, 230, 220)
 RCOLOR = (199, 117, 61)
 SCOLOR = (0, 255, 255)
 TLENGTH = 600
-CLENGTH = TLENGTH / 8
+CLENGTH = TLENGTH // 8
 FPS = 60
+_PNAMES = ["bB", "bK", "bN", "bp", "bQ", "bR", "wB", "wK", "wN", "wp", "wQ", "wR"]
+IMAGES = {i: pygame.transform.scale(pygame.image.load("images/" + i + ".png"), (CLENGTH, CLENGTH)) for i in _PNAMES}
+
